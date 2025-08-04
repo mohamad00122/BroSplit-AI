5db4ee2682a4f2304c7d7e268e01f25eebc98573
BigDaddy57 <mohamad00122@gmail.com>
2025-08-04 12:18:34 -0700

    updated stripe to

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/api/checkout", async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       line_items: [{
-        price: 'price_1RsQJUAhLaqVN2Rssepup9EE',
+        price: 'price_1RrLHFAhLaqVN2RsuypXJYSA',
         quantity: 1
       }],
       mode: "payment",
